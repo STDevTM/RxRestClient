e47fa7fe0d3bf726fc7f19da7d466cc74685865c
Hovak Davtyan <hovak.stdev@gmail.com>
2019-05-23 13:43:22 +0400

    Version 1.2.0 Updated Swift version to 5 Updated RxSwift to 5

--- RxRestClient/Classes/ReachabilityService.swift
+++ RxRestClient/Classes/ReachabilityService.swift
@@ -74,9 +74,9 @@ class DefaultReachabilityService: ReachabilityService {
 }
 
 extension ObservableConvertibleType {
-    func retryOnBecomesReachable(_ valueOnFailure: E, reachabilityService: ReachabilityService) -> Observable<E> {
+    func retryOnBecomesReachable(_ valueOnFailure: Element, reachabilityService: ReachabilityService) -> Observable<Element> {
         return self.asObservable()
-            .catchError { (e) -> Observable<E> in
+            .catchError { (e) -> Observable<Element> in
                 reachabilityService.reachability
                     .skip(1)
                     .filter { $0.reachable }
