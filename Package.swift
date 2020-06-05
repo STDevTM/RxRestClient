25f6cdeed823b6ca170ea059e4177ec9b2a52ad2
Aram Semerjyan <semerjyan.aram.stdev@gmail.com>
2020-06-05 13:05:54 +0400

    Added dependencies for RxRestClient package.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.1.0")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.1.0")),
+        .package(url: "https://github.com/RxSwiftCommunity/RxAlamofire.git", .upToNextMajor(from: "5.3.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
