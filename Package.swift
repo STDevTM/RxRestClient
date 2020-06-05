e4022590bb8deafe257fcee5410f3e8bf6d02b8e
Aram Semerjyan <semerjyan.aram.stdev@gmail.com>
2020-06-05 13:19:55 +0400

    Added platform info.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "RxRestClient",
+    platforms: [
+      .iOS(.v10),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -22,7 +25,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RxRestClient",
-            dependencies: []),
+            dependencies: [
+                .product(name: "RxSwift", package: "RxSwift"),
+                .product(name: "Alamofire", package: "Alamofire"),
+                .product(name: "RxAlamofire", package: "RxAlamofire")
+            ]),
         .testTarget(
             name: "RxRestClientTests",
             dependencies: ["RxRestClient"]),
